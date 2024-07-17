b26184030fd317289c6ec16430669de0e8c54d0c
Younggil An <nilgil183@gmail.com>
2024-07-17 23:14:09 +0900

    Title: 바이러스 검사, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240717/바이러스 검사/virus-detector.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] counts = new int[n];
+        for (int i = 0; i < n; i++) {
+            counts[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int leader = Integer.parseInt(st.nextToken());
+        int crew = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result++;
+            counts[i] -= leader;
+
+            if (counts[i] <= 0) {
+                result += counts[i] / crew;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
