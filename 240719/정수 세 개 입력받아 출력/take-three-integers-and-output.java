93e0420dc4ee829f53821734fe40c8c0b9cce96c
Younggil An <nilgil183@gmail.com>
2024-07-19 02:13:36 +0900

    Title: 정수 세 개 입력받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240719/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
