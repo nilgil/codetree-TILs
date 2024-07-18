7d3253c93a1d176419ee0d3382349c5eef806bba
Younggil An <nilgil183@gmail.com>
2024-07-19 02:10:10 +0900

    Title: 실수 입력받아 계산 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240719/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        System.out.println(a + 1.5);
+    }
+}
