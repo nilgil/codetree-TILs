2553d8380585cc823a987b43ef7b0919a4f1e787
Younggil An <nilgil183@gmail.com>
2024-07-19 02:06:05 +0900

    Title: 데이터 교환, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

--- 240719/데이터 교환/exchange-data.java
+++ 240719/데이터 교환/exchange-data.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c = 7;
+
+        int tmp = b;
         b = a;
-        c = b;
+        a = tmp;
+
+        tmp = a;
         a = c;
+        c = tmp;
+
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
