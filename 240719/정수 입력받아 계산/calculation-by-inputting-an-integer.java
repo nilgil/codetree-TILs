c86ca1ceba9e3ea11cd0bfd41b006845ecac52d8
Younggil An <nilgil183@gmail.com>
2024-07-19 02:08:51 +0900

    Title: 정수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240719/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a * 2 + 3);
+    }
+}
