95873440f3cb0446eff443cc1c0a63f3f58ffd51
Younggil An <nilgil183@gmail.com>
2024-07-19 20:11:02 +0900

    Title: 정사각형의 넓이, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

--- 240719/정사각형의 넓이/area-of-a-rectangle.java
+++ 240719/정사각형의 넓이/area-of-a-rectangle.java
@@ -7,8 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        n = n * n;
-        System.out.println(n);
+        System.out.println(n * n);
         if (n < 5) {
             System.out.println("tiny");
         }
